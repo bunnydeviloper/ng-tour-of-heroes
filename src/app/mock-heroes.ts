38dffe82bf306b533678f17e53e4e91ea8f275a2
Sophia <phuongmai001@gmail.com>
2018-10-11 15:26:50 -0700

    style: add styling for heroes component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: "Magneto" },
+  { id: 2, name: "Wonder Women" },
+  { id: 3, name: "Aqua Man" },
+  { id: 4, name: "Thor" },
+  { id: 5, name: "Flash" },
+  { id: 6, name: "Spider Man" },
+  { id: 7, name: "Super Man" },
+  { id: 8, name: "Lucky" },
+  { id: 9, name: "The Hulk" },
+  { id: 10, name: "Anita B" },
+];
