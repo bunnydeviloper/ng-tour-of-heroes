b19b131c819b7089c8642910b5e677bbc7ab19a3
Sophia <phuongmai001@gmail.com>
2018-10-11 15:47:25 -0700

    feat: click on each hero to see the details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,16 +8,16 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Ironman"
-  };
 
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
